0ff8b3d9572f282032af1ddcbef09bcbecec4c70
sonprolatao056 <57830932+sonprolatao056@users.noreply.github.com>
2021-07-09 14:59:38 +0700

    cart api

--- Controllers/Api/CartController.cs
+++ Controllers/Api/CartController.cs
@@ -1,15 +1,54 @@
-﻿using System;
+﻿using MicroWaveFood.Dtos;
+using MicroWaveFood.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace MicroWaveFood.Controllers.Api
 {
     public class CartController : ApiController
     {
-        
+        [HttpPost]
+        public IHttpActionResult Add(ProductDto product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            List<Cart> list = TakeCart();
+            //check if this product is in session["cart"]
+            Cart cart = list.Find(a => a.ProductId == product.ProductId);
+            if (cart == null)
+            {
+                cart = new Cart(product.ProductId);
+                list.Add(cart);
+            }
+            else
+            {
+                cart.Amount++;
+            }
+
+            return Ok();
+        }
+        private List<Cart> TakeCart()
+        {
+            //List<Cart> listCart = Session["Cart"] as List<Cart>;
+            List<Cart> listCart = ListCart.Carts;
+            if (listCart == null)
+            {
+                //if list cart doesn't exist, create one
+                listCart = new List<Cart>();
+                //Session["Cart"] = listCart;
+                ListCart.Carts = listCart;
+            }
+            return listCart;
+        }
+
+
     }
 }
---
+++ Dtos/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroWaveFood.Dtos
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+    }
+}
